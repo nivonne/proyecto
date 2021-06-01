9034f53e7e1a9f7b595b4137ac9513f67d0cf547
nivonne <nimera27@gmail.com>
2021-06-01 17:37:59 -0500

    Creé mi formulario para crear notas, creé la ruta para renderizar mi formulario y tambíen la ruta post para obtener los datos que el usuario escriba, además agregué condicionales en caso de que exista un error y en mi formulario inserté alertas de bootstrap para mostrarle al ususario el error

--- src/routes/notas.js
+++ src/routes/notas.js
@@ -1,6 +1,31 @@
 const {Router}= require('express');
 const router=Router();
 
+router.get('/notes/add', (req,res)=>{
+    res.render('notes/new-note');
+});
+
+router.post('/notes/new-note', (req,res)=>{
+    const {title,description}=req.body;
+    const errors=[];
+    if(!title){
+        errors.push({text:'Please write a title'});
+    }
+    if(!description){
+        errors.push({text:'Please write a description'});
+    }
+    if(errors.length >0){
+        res.render('notes/new-note', {
+            errors,
+            title,
+            description
+        });
+    }else{
+        res.send('ok');
+    }
+    
+});
+
 router.get('/notes', (req,res)=>{
     res.send('Notas');
 });
