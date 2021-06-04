bdd0894aa72629cea47c7531f566d447bd86f3d2
nivonne <nimera27@gmail.com>
2021-06-03 21:54:06 -0500

    Cree mi ruta post para recibir los datos del ususario cuando se registra, le coloqué condicinales para validar que la contraseña y la confirmación sean iguales, que la contraseña sea mayor a cuatro dígitos y y que no existan errores, además agregué en mi formulario, la ruta y el método

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -9,4 +9,22 @@ router.get('/users/signup', (req, res)=>{
     res.render('users/signup');
 });
 
+router.post('/users/signup', (req, res)=>{
+    console.log(req.body);
+    const{name,email,password,confirm_password}=req.body;
+    const errors=[];
+    if(password!= confirm_password){
+        errors.push({text:'Password do not match'});
+    }
+    if(password.length < 4){
+        errors.push({text:'Password must be at least 4 characters'});
+    }
+    if(errors.length > 0){
+        res.render('users/signup', {errors, name, email, password, confirm_password});
+    }else{
+        res.send('ok');
+    }
+    
+})
+
 module.exports=router;
