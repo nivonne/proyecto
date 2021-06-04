ae63051d8cbfce9e6b40db22073b1567b92cfed3
nivonne <nimera27@gmail.com>
2021-06-03 21:03:12 -0500

    Agregué flash en mi configuración del servidor, así como en mis rutas post,put y delete y creé el archivo messages para poder poner la alerta de bootstrap y en mi vista pincipal inserté mi partial

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const path=require('path');
 const exphbs=require('express-handlebars');
 const methodOverride=require('method-override');
 const session=require('express-session');
+const flash=require('connect-flash');
 require('./database');
 const Handlebars=require('handlebars');
 const{
@@ -33,6 +34,14 @@ app.use(session({
     resave:true,
     saveUninitialized:true
 }));
+app.use(flash());
+
+app.use((req, res, next)=>{
+    res.locals.success_msg=req.flash('success_msg');
+    res.locals.error_msg=req.flash('error_msg');
+
+    next();
+})
 
 //Routes
 app.use(require('./routes/index'));
--- src/routes/notas.js
+++ src/routes/notas.js
@@ -25,6 +25,7 @@ router.post('/notes/new-note', async (req,res)=>{
     }else{
        const newNote= new Note({title,description});
         await newNote.save();
+        req.flash('success_msg','Note Added Successfully')
        res.redirect('/notes');
     }
     
@@ -43,12 +44,13 @@ router.get('/notes/edit/:id',async (req, res)=>{
 router.put('/notes/edit-note/:id', async(req, res)=>{
     const {title,description}= req.body;
     await Note.findByIdAndUpdate(req.params.id,{title,description});
+    req.flash('success_msg', 'Note Updated Successfully');
     res.redirect('/notes');
 });
 
 router.delete('/notes/delete/:id', async (req,res)=>{
     await Note.findByIdAndDelete(req.params.id);
-    console.log(req.params.id);
+    req.flash('success_msg', 'Note Deleted Successfully');
     res.redirect('/notes');
 });
 
