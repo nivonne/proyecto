090ea347964ca32145b1ca2f8cf1afecb84fedb0
nivonne <nimera27@gmail.com>
2021-06-04 20:52:33 -0500

    Agregué a mi archivo de rutas, mi modelo y en lugar de mostrar un ok al registrarme agregué un condicional para que me avise si ese correo ya esta usado y me recireccioné a el login, y si no está registrado, que se cree un nuevo usuario en mi base de datos, también que me encripte la contraseña y ya después se guarde,  un mensaje de que fue registrado y redireccionar al login

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -1,6 +1,8 @@
 const {Router}= require('express');
 const router=Router();
 
+const User=require('../models/User');
+
 router.get('/users/signin', (req,res) =>{
 res.render('users/signin');
 });
@@ -9,8 +11,7 @@ router.get('/users/signup', (req, res)=>{
     res.render('users/signup');
 });
 
-router.post('/users/signup', (req, res)=>{
-    console.log(req.body);
+router.post('/users/signup',async (req, res)=>{
     const{name,email,password,confirm_password}=req.body;
     const errors=[];
     if(name.length <=0 && email.length <=0){
@@ -25,7 +26,16 @@ router.post('/users/signup', (req, res)=>{
     if(errors.length > 0){
         res.render('users/signup', {errors, name, email, password, confirm_password});
     }else{
-        res.send('ok');
+        const emailUser=await User.findOne({email:email});
+        if(emailUser){
+            req.flash('error_msg', 'The email is already in use');
+            res.redirect('/users/signup');
+        }   
+        const newUser=new User({name,email,password});
+        newUser.password=await newUser.encryptPassword(password);
+        await newUser.save();
+        req.flash('success_msg', 'You are registered');
+        res.redirect('/users/signin');
     }
     
 })
